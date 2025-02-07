865daaf3d431ded1f3fad19f9e8a80a682f08d9f
lucadileo9 <lucadileo70@gmail.com>
2025-02-07 10:56:39 +0100

    Handling events
    Vari modi di scrivere funzioni per gestire eventi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Heading from './Heading';
+import Btn from './Btn';
 function App() { 
   return ( 
     <div className="App"> 
       <Heading /> 
+      <Btn />
     </div> 
   ); 
 } 
