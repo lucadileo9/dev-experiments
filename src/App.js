ef69c71aba4682bcc9e97566a66d3c3d361549c3
lucadileo9 <lucadileo70@gmail.com>
2025-02-07 11:26:46 +0100

    use of variables
    La gestione degli eventi può essere usata per cambiare valori di variabili

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Heading from './Heading';
 import Btn from './Btn';
+import GuessNumber from './GuessNumber';
 function App() { 
   return ( 
     <div className="App"> 
       <Heading /> 
-      <Btn />
+      {/* <Btn /> */}
+      <GuessNumber />
     </div> 
   ); 
 } 
