cbfa64648f7ac0be73c9217378380650cd3297e4
lucadileo9 <lucadileo70@gmail.com>
2025-02-08 11:19:52 +0100

    routing
    Ecco come va gestito il routing in react

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import './App.css';
 import Heading from './Heading';
 import Btn from './Btn';
 import GuessNumber from './GuessNumber';
+import Contact from './Contact';
+import Homepage from './Homepage';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
 function App() { 
   return ( 
+    <BrowserRouter> {/* Avvolgi l'applicazione con BrowserRouter */}
     <div className="App"> 
-      <Heading /> 
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       {/* <Btn /> */}
-      <GuessNumber />
     </div> 
+    </BrowserRouter> // Chiudi il tag BrowserRouter
   ); 
 } 
  
---
+++ src/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Heading from './Heading';
+import GuessNumber from './GuessNumber';
+function Homepage() {
+  return (
+    <div>
+      <Heading />
+      <GuessNumber />
+    </div>
+  );
+}
+
+export default Homepage;
