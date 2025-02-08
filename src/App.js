fe790d2a4349199f6827b2bfe210204c524e5076
lucadileo9 <lucadileo70@gmail.com>
2025-02-08 11:50:34 +0100

    video player

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Btn from './Btn';
 import GuessNumber from './GuessNumber';
 import Contact from './Contact';
 import Homepage from './Homepage';
+import Video from './Video';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 function App() { 
@@ -13,11 +14,13 @@ function App() {
     <nav>
       <Link to="/">Home</Link>
       <Link to="/contact">Contact</Link>
+      <Link to="/video">Video</Link>
     </nav>
 
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/contact" element={<Contact />} />
+        <Route path="/video" element={<Video />} />
       </Routes>
       {/* <Btn /> */}
     </div> 
