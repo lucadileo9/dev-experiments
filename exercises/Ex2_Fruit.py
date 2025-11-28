fa6f98e5012154b76f7e73d2a2e2383105728fa2
lucadileo9 <lucadileo70@gmail.com>
2025-11-28 16:43:12 +0100

    resolve exercise 2

--- exercises/Ex2_Fruit.py
+++ exercises/Ex2_Fruit.py
@@ -22,11 +22,45 @@ from z3 import *
 
 # Create 3 integer variables
 apple, banana, carrot = Ints('apple banana carrot')
+solve(
+      apple >= 1,   # at least one apple
+      banana >= 1,   # at least one banana
+      carrot >= 1,   # at least one carrot
+      banana + apple + carrot == 100, # exactly 100 fruits overall
+      banana *2 + apple + carrot * 0.5 == 100 # final cost must be 100€
+      )
+# 2.1 b
+s = Solver()
+s.add(
+      apple >= 1,   # at least one apple
+      banana >= 1,   # at least one banana
+      carrot >= 1,   # at least one carrot
+      banana + apple + carrot == 100, # exactly 100 fruits overall
+      banana *2 + apple + carrot * 0.5 == 100 # final cost must be 100€
+)
+if s.check() == sat:
+    model1 = s.model()
+    print("Prima soluzione:", model1)
+    
+    s.add(Or(apple != model1[apple], 
+             banana != model1[banana],
+             carrot != model1[carrot]))
+        
+    if s.check() == sat:
+        print("Esiste più di una soluzione!")
+        print("Seconda soluzione:", s.model())
+    else:
+        print("Esiste una sola soluzione")
+else:
+    print("Nessuna soluzione trovata")
+
+
+# 2.1.c
 solve(apple >= 1,   # at least one apple
-      ... ,   # at least one banana
-      ... , # at least one carrot
-      ... , # exactly 100 fruits overall
-      ... # final cost must be 100€
+      banana >= 40,   # at 40 banana
+      carrot >= 1,   # at least one carrot
+      banana + apple + carrot == 100, # exactly 100 fruits overall
+      banana *2 + apple + carrot * 0.5 == 100 # final cost must be 100€
       )
 
 
@@ -44,3 +78,34 @@ Ex 2.2.a How many pine nuts, ananas, and limes can they buy to satisfy the previ
 
 Ex 2.2.b Is there more than a way to buy the fruit satisfying the previous conditions?
 """
+pine, ananas, lime = Ints('pine ananas lime')
+solve(pine >= 1,   # at least one pine
+      ananas >= 1,   # at least one ananas
+      lime >= 1,   # at least one lime
+      pine + ananas + lime == 100, # exactly 100 fruits overall
+      pine *40 + ananas + lime * 0.5 == 100 # final cost must be 100€
+      )
+
+# 2.2
+s = Solver()
+s.add(pine >= 1,
+      ananas >= 1,
+      lime >= 1,
+      pine + ananas + lime == 100,
+      pine * 40 + ananas + lime * 0.5 == 100)
+
+if s.check() == sat:
+    model1 = s.model()
+    print("Prima soluzione:", model1)
+    
+    s.add(Or(pine != model1[pine], 
+             ananas != model1[ananas], 
+             lime != model1[lime]))
+    
+    if s.check() == sat:
+        print("Esiste più di una soluzione!")
+        print("Seconda soluzione:", s.model())
+    else:
+        print("Esiste una sola soluzione")
+else:
+    print("Nessuna soluzione trovata")
