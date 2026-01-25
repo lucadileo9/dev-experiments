d97b4baf696876542dcf1cfb06ba3a1a53e98804
lucadileo9 <lucadileo70@gmail.com>
2026-01-25 09:34:24 +0100

    update useful files

--- django_project/production_settings.py
+++ django_project/production_settings.py
@@ -1,13 +1,22 @@
+import os
 from django_project.settings import *
-
+# import pymysql
+# pymysql.install_as_MySQLdb()
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'blog',
-        'USER': 'django',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE', 'blog'),
+        'USER': os.environ.get('MYSQL_USER', 'django'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD', 'django_password'),
+        'HOST': os.environ.get('MYSQL_HOST', 'db'),  # Nome servizio Docker
+        'PORT': os.environ.get('MYSQL_PORT', '3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
+
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'temp-key')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'web']
