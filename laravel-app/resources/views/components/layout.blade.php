a00624c6909ccf369a897a37c20e6c6f2f9457ca
lucadileo9 <lucadileo70@gmail.com>
2026-03-28 10:09:34 +0100

    refactor: layout using components

--- laravel-app/resources/views/components/layout.blade.php
+++ laravel-app/resources/views/components/layout.blade.php
@@ -3,40 +3,30 @@
 ])
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-theme="dim">
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
-    {{-- <link rel="stylesheet" href="./style.css"> --}}
-    <!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> -->
+    
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-
 
     <link rel="icon" href="./favicon.ico" type="image/x-icon">
   </head>
-  <body class="bg-gray-900 text-white">
-    <main class="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8">
-    <nav class="mb-8">
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
-            <li><a href="/ideas">Ideas</a></li>
-            <li><a href="/register">Register</a></li>
-            <li><a href="/login">Login</a></li>
-            @if(auth()->check())
-            <form action="{{ route('logout') }}" method="POST" class="inline">
-                @csrf
-                <button type="submit" class="text-white hover:text-gray-300">Logout</button>
-            </form>
-            @endif
-        </ul>
-    </nav>
+  <body class="bg-base-100 text-base-content flex flex-col min-h-screen">
+    
+    <x-navbar />
+    <x-flash />
 
-     {{ $slot }}
+    <main class="flex-grow mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8 w-full">
+        {{ $slot }}
     </main>
-    <script src="index.js"></script>
+
+    <footer class="footer footer-center p-4 bg-base-300 text-base-content mt-auto">
+        <aside>
+            <p>Copyright © {{ date('Y') }} - All rights reserved by DevExperiments</p>
+        </aside>
+    </footer>
   </body>
 </html>
