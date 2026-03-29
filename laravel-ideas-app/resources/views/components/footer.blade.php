1acd23ee646855ab317d0f5b2ecc40571783685e
lucadileo9 <lucadileo70@gmail.com>
2026-03-29 12:00:06 +0200

    feat: add footer

---
+++ laravel-ideas-app/resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-white border-t mt-auto">
+    <div class="container mx-auto px-4 py-6 text-center text-gray-500 text-sm">
+        &copy; {{ date('Y') }} Ideas App. All rights reserved.
+    </div>
+</footer>
