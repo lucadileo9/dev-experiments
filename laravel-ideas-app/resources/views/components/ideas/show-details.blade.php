964f1d98db7a24a6e189887ea31f14c638494717
lucadileo9 <lucadileo70@gmail.com>
2026-03-30 20:35:02 +0200

    feat: add dedicated component for idea show

---
+++ laravel-ideas-app/resources/views/components/ideas/show-details.blade.php
@@ -0,0 +1,7 @@
+@props(['description'])
+
+<div class="mt-6">
+    <div class="bg-base-100 p-6 rounded-box shadow-inner mb-6">
+        <p class="text-xl text-base-content leading-relaxed">{{ $description }}</p>
+    </div>
+</div>
