421b10e5fc4bd9c4f04feb3bd6ac9d2339934f11
Luca Di Leo <lucadileo70@gmail.com>
2026-03-30 10:12:33 +0200

    minor change

--- laravel-ideas-app/app/Models/User.php
+++ laravel-ideas-app/app/Models/User.php
@@ -7,6 +7,7 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -30,7 +31,7 @@ class User extends Authenticatable
         ];
     }
 
-    public function ideas()
+    public function ideas(): HasMany
     {
         return $this->hasMany(Idea::class);
     }
