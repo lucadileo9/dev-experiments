cbcff87beab90cff0a56351254afd6484aadb6cf
Luca Di Leo <lucadileo70@gmail.com>
2026-03-30 10:52:20 +0200

    feat: add flash message disappearing

--- laravel-ideas-app/resources/views/components/ui/flash.blade.php
+++ laravel-ideas-app/resources/views/components/ui/flash.blade.php
@@ -1,5 +1,16 @@
 @if (session('success'))
-    <div class="toast toast-top toast-center z-[100] mt-16">
+    <div
+        class="toast toast-top toast-center z-[100] mt-16"
+        x-data="{ show: true }"
+        x-show="show"
+        x-init="setTimeout(() => show = false, 3000)"
+        x-transition:enter="transition ease-out duration-300"
+        x-transition:enter-start="opacity-0 translate-y-2"
+        x-transition:enter-end="opacity-100 translate-y-0"
+        x-transition:leave="transition ease-in duration-300"
+        x-transition:leave-start="opacity-100 translate-y-0"
+        x-transition:leave-end="opacity-0 translate-y-2"
+    >
         <div class="alert alert-success shadow-lg">
             <span>{{ session('success') }}</span>
         </div>
